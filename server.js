c32f7304b1ce106a1fa11f468006c86bf0919cfe
robdan89 <fuquardwork@gmail.com>
2021-02-10 12:36:05 -0600

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Without bringing in babel or something like that you can't use 'import' syntax. You use this instead...
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -10,14 +11,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the contact keeper API..." })
-);
-
 // Defin Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production (serve React in production)
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
